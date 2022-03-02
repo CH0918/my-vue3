58cc5bddfb61e1af9d4b008277780ac03f237695
dongjiang.huang <827316877@qq.com>
2022-03-02 11:11:00 +0800

    feat 实现isRef,unRef,proxyRefs

--- src/reactivity/__tests__/ref.test.ts
+++ src/reactivity/__tests__/ref.test.ts
@@ -1,6 +1,6 @@
 import { effect } from '../src/effect';
 import { reactive } from '../src/reactive';
-import { ref } from '../src/ref';
+import { ref, proxyRefs, isRef, unRef } from '../src/ref';
 describe('ref', () => {
   it('should be reactive', () => {
     const a = ref(1);
@@ -35,38 +35,38 @@ describe('ref', () => {
     expect(dummy).toBe(2);
   });
 
-  // it.skip('proxyRefs', () => {
-  //   const user = {
-  //     age: ref(10),
-  //     name: 'xiaohong',
-  //   };
-  //   const proxyUser = proxyRefs(user);
-  //   expect(user.age.value).toBe(10);
-  //   expect(proxyUser.age).toBe(10);
-  //   expect(proxyUser.name).toBe('xiaohong');
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: 'xiaohong',
+    };
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe('xiaohong');
 
-  //   (proxyUser as any).age = 20;
-  //   expect(proxyUser.age).toBe(20);
-  //   expect(user.age.value).toBe(20);
+    (proxyUser as any).age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
 
-  //   proxyUser.age = ref(10);
-  //   expect(proxyUser.age).toBe(10);
-  //   expect(user.age.value).toBe(10);
-  // });
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
+  });
 
-  // it.skip('isRef', () => {
-  //   const a = ref(1);
-  //   const user = reactive({
-  //     age: 1,
-  //   });
-  //   expect(isRef(a)).toBe(true);
-  //   expect(isRef(1)).toBe(false);
-  //   expect(isRef(user)).toBe(false);
-  // });
+  it('isRef', () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
 
-  // it.skip('unRef', () => {
-  //   const a = ref(1);
-  //   expect(unRef(a)).toBe(1);
-  //   expect(unRef(1)).toBe(1);
-  // });
+  it('unRef', () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
 });
--- src/reactivity/src/ref.ts
+++ src/reactivity/src/ref.ts
@@ -4,8 +4,9 @@ import { reactive } from './reactive';
 class RefImpl {
   private _value;
   dep = new Set();
+  __v_isRef_ = true;
   constructor(value) {
-    this._value = isObject(value) ? reactive(value) : value;
+    this._value = convert(value);
   }
   get value() {
     if (isTracking()) {
@@ -15,13 +16,42 @@ class RefImpl {
   }
   set value(newValue) {
     if (!hasChange(this.value, newValue)) return;
-    this._value = isObject(newValue) ? reactive(newValue) : newValue;
+    this._value = convert(newValue);
     triggerEffect(this.dep);
   }
+}
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
 }
 export function hasChange(value, newValue) {
   return !Object.is(value, newValue);
 }
 export function ref(value) {
   return new RefImpl(value);
+}
+export function isRef(value) {
+  return !!value.__v_isRef_;
+}
+export function unRef(value) {
+  return isRef(value) ? value.value : value;
+}
+const shallowUnwrapHandlers = {
+  get(target, key) {
+    return unRef(Reflect.get(target, key));
+  },
+  set(target, key, value) {
+    // 1.old -> ref; new -> !ref 需要.value再赋值
+    // 2.old -> ref; new -> ref
+    // 3.old -> !ref; new -> ref
+    // 4.old -> !ref; new -> !ref
+    const oldValue = target[key];
+    if (isRef(oldValue) && !isRef(value)) {
+      return (target[key].value = value);
+    } else {
+      return Reflect.set(target, key, value);
+    }
+  },
+};
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, shallowUnwrapHandlers);
 }
